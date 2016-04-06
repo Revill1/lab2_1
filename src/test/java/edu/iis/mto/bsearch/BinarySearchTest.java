d6e9352491d0c344785d2e2d5c9b27c41920d840
Revill1 <adamredlinski@gmail.com>
2016-04-06 18:18:19 +0200

    Zaimplementowane rzucanie wyjatku IllegalArgumentException oraz dodany test wyjatku

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -10,65 +10,59 @@ public class BinarySearchTest {
 
 	@Test
 	public void test() {
-		int[] seq =  new int[]{5};
-		SearchResult result = BinarySearch.search(5,seq);
-		assertThat(result.isFound(),equalTo(true));
+		int[] seq = new int[] { 5, 6, 8 };
+		SearchResult result = BinarySearch.search(5, seq);
+		assertThat(result.isFound(), equalTo(true));
 	}
-	
+
 	@Test
 	public void testFoundNot() {
-		int[] seq =  new int[]{4};
-		SearchResult result = BinarySearch.search(5,seq);
-		assertThat(result.isFound(),equalTo(false));
+		int[] seq = new int[] { 4, 6 };
+		SearchResult result = BinarySearch.search(5, seq);
+		assertThat(result.isFound(), equalTo(false));
 	}
-	
+
 	@Test
 	public void testMoreThanOneIsFirst() {
-		int[] seq =  new int[]{4,5,7,8,9};
-		SearchResult result = BinarySearch.search(4,seq);
-		assertThat(result.isFound(),equalTo(true));
-		assertThat(result.getPosition(),equalTo(1));
+		int[] seq = new int[] { 4, 5, 7, 8, 9 };
+		SearchResult result = BinarySearch.search(4, seq);
+		assertThat(result.isFound(), equalTo(true));
+		assertThat(result.getPosition(), equalTo(1));
 	}
-	
+
 	@Test
 	public void testMoreThanOneisLast() {
-		int[] seq =  new int[]{4,5,7,8,9};
-		SearchResult result = BinarySearch.search(9,seq);
-		assertThat(result.isFound(),equalTo(true));
-		assertThat(result.getPosition(),equalTo(seq.length));
+		int[] seq = new int[] { 4, 5, 7, 8, 9 };
+		SearchResult result = BinarySearch.search(9, seq);
+		assertThat(result.isFound(), equalTo(true));
+		assertThat(result.getPosition(), equalTo(seq.length));
 	}
-	
+
 	@Test
 	public void testMoreThanOneIsMiddle() {
-		int[] seq =  new int[]{4,5,7,8,9};
-		SearchResult result = BinarySearch.search(7,seq);
-		assertThat(result.isFound(),equalTo(true));
-		assertThat(result.getPosition(),equalTo((int) seq.length / 2 + 1));
+		int[] seq = new int[] { 4, 5, 7, 8, 9 ,10};
+		SearchResult result = BinarySearch.search(7, seq);
+		assertThat(result.isFound(), equalTo(true));
+		if (seq.length % 2 == 0) {
+			assertThat(result.getPosition(), equalTo((int) seq.length / 2));
+		} else {
+			assertThat(result.getPosition(), equalTo((int) seq.length / 2 + 1));
+		}
 	}
-	
+
 	@Test
 	public void testMoreThanOneIsNotIn() {
-		int[] seq =  new int[]{4,5,7,8,9};
-		SearchResult result = BinarySearch.search(3,seq);
-		assertThat(result.isFound(),equalTo(false));
+		int[] seq = new int[] { 4, 5, 7, 8, 9 };
+		SearchResult result = BinarySearch.search(3, seq);
+		assertThat(result.isFound(), equalTo(false));
 	}
-	
-	@Test
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsException() {
-		int[] seq =  new int[]{};
+		int[] seq = new int[] {};
 		boolean throwed = false;
-		try{
-			SearchResult result = BinarySearch.search(4, seq);
-			assertThat(result.isFound(),equalTo(true));
-		}
-		catch(IllegalArgumentException x)
-		{
-			throwed = true;
-		}
-		assertTrue(throwed);
-		
+
+		SearchResult result  = BinarySearch.search(3,seq);
 	}
-	
-	
 
 }
